b3bf423adae059329133c57481f8fc9955053a2e
Athul Sanjose <59477486+UltimateRoman@users.noreply.github.com>
2021-05-28 19:58:35 +0530

    Created Navbar and Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import { ReactNavbar } from "react-responsive-animate-navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
+import logo1 from '../logo.png';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <h1>Arcello</h1>
-        </nav>
+      <div style={{height:800}}>
+        <Router>
+          <ReactNavbar
+          color="rgb(240,248,255)"
+          logo={logo1}
+          menu={[
+            { name: "HOME", to: "/" }
+          ]}
+          social={[]}
+          />
+          <Route exact path="/"/>
+        </Router>
       </div>
     );
   }
