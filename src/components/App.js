0aece3cf24fa899b8318f42c4c7f24186b80679a
Athul Sanjose <59477486+UltimateRoman@users.noreply.github.com>
2021-05-28 21:07:40 +0530

    Added support for Celo

--- src/components/App.js
+++ src/components/App.js
@@ -15,15 +15,15 @@ class App extends Component {
   }
 
   async loadWeb3() {
-    if (window.ethereum) {
-      await window.ethereum.enable()
+    if (window.celo) {
+      await window.celo.enable()
       window.web3 = new Web3(window.ethereum)
     }
     else if (window.web3) {
       window.web3 = new Web3(window.web3.currentProvider)
     }
     else {
-      window.alert('Use the Metamask Wallet!')
+      window.alert('Use the Celo Extension Wallet!')
     }
   }
 
