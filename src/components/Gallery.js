7f2072cb58aaeb01fbf53c86d2a8fd752f9c9240
fazil47 <fazil47babu@gmail.com>
2021-05-29 21:53:54 +0530

    Added gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,11 +3,69 @@ import './App.css';
 
 class Gallery extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            search: ''
+        };
+    }
+
+    updateSearch(event) {
+        this.setState({ search: event.target.value.substr(0, 20) });
+    }
+
     render() {
+
+        let filteredAssets = this.props.assets.filter(
+            (asset) => {
+                return asset.name.indexOf(this.state.search) !== -1;
+            }
+        );
         return (
-            <div className="contain">
-                <h1 align="center" style={{ marginTop: '100px' }} class="w3-tangerine">Arcello</h1>
-                <h3 align="center"> Team Coding-Fortress </h3>
+            <div className="container-fluid mt-5">
+                <div className="row">
+                    <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '500px' }}>
+                        {filteredAssets.map((asset, key) => {
+                            return (
+                                <div class="coupon" key={key} >
+                                    <div className="card-header">
+                                        <p class="badge badge-primary">{asset.name}</p>
+                                        <br />
+                                        <div className="sketchfab-embed-wrapper">
+                                            <iframe title="SWAT operator" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="fullscreen; autoplay; vr" src={asset.fileid} />
+                                        </div>
+                                        <br />
+                                        <small>Creator: {asset.creator}</small>
+                                    </div>
+                                    <ul id="postList" className="list-group list-group-flush">
+                                        <li key={key} className="list-group-item py-2">
+                                            <small className="float-left mt-1 text-muted">
+                                                Price: {window.web3.utils.fromWei(asset.price.toString(), 'Ether')} CELO
+                                </small>
+                                            <form onSubmit={(event) => {
+                                                event.preventDefault()
+                                                const tid = asset.id
+                                                const amount = this.bidamount.value
+                                                this.props.createBid(tid, amount)
+
+                                            }}>
+                                                <input
+                                                    id="bidamount"
+                                                    type="text"
+                                                    ref={(input) => { this.bidamount = input }}
+                                                    className="form-control"
+                                                    placeholder="Bid Amount"
+                                                    required
+                                                />
+                                                <button type="submit" className="btn btn-outline-info">Create Bid</button>
+                                            </form>
+                                        </li>
+                                    </ul>
+                                </div>
+                            )
+                        })}
+                    </main>
+                </div>
             </div>
         );
     }
--- truffle-config.js
+++ truffle-config.js
@@ -1,31 +1,31 @@
 require('babel-register');
 require('babel-polyfill');
-/*const fs = require('fs');
+const fs = require('fs');
 
 const Kit = require('@celo/contractkit')
 const kit = Kit.newKit('https://alfajores-forno.celo-testnet.org')
 
 const getAccount = require('./getAccount').getAccount
 
-async function awaitWrapper(){
-    let account = await getAccount()
-    kit.connection.addAccount(account.privateKey)
+async function awaitWrapper() {
+  let account = await getAccount()
+  kit.connection.addAccount(account.privateKey)
 }
-awaitWrapper()*/
+awaitWrapper()
 
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "*" 
-    }/*,
+      network_id: "*"
+    },
     alfajores: {
       provider: kit.connection.web3.currentProvider, 
       network_id: 44787 
-    }*/
+    }
   },
-  
+
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
   compilers: {
